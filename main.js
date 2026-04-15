c545d273339986ba244ee93a8bea9e83f43ef11e
samusenai <samoel.cardoso@escola.pr.gov.br>
2026-04-15 20:52:57 -0300

    algumas mudanças no código, substiruição de If por switch case

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
+console.log("adição- 1");
+console.log("subtração- 2");
+console.log("multiplicação- 3");
+console.log("divisão- 4");
+console.log("bhaskara- B");
+let Defdocal = prompt("qual o calculo desejado?(olhar no console para informações das palavras chave para definir as contas)");
 
-console.log("adição");
-console.log("subtração");
-console.log("multiplicação");
-console.log("divisão");
-console.log("bhaskara");
-let Defdocal = prompt("qual o calculo desejado?(olhar no terminal para informações das palavras para definir as contas)");
 
-
-if(Defdocal == "bhaskara"){
+if(Defdocal == "B"){
 let bhaa = Number(prompt("digite o A da função"));
 let bhab = Number(prompt("digite o B da função"));
 let bhac = Number(prompt("digite o C da função"));
@@ -16,45 +15,42 @@ const BHASM = (-bhab+Math.sqrt(DELTA))/2*bhaa;
 const BHASN = (-bhab-Math.sqrt(DELTA))/2*bhaa;
 alert("("+BHASN+","+ BHASM+")");
 }
-
-if(Defdocal == "adição"){
-    console.log("a conta de soma foi selecionada, ela calcula até 5 números.");
-let numa = Number(prompt("digite o primeiro número a somar"));
-let numb = Number(prompt("digite o segundo número a somar"));
-let numc = Number(prompt("digite o terceiro número a somar"));
-let numd = Number(prompt("digite o quarto número a somar"));
-let nume = Number(prompt("digite o quinto número a somar"));
-const somares = numa+numb+numc+numd+nume;
-alert("a soma dos números deu "+ somares);
+switch(Defdocal){
+case Defdocal == "1" :
+    let soma = 0;
+    let n = Number(prompt("número de números a calcular"));
+    for( i=0; i<n; i++ ) {
+    var numero = Number(prompt("digite o número a somar/ser somado"));
+    soma = soma + numero; 
+    console.log("a soma total deu "+ soma)};
 console.log("programa finalizado, favor recarregar a página para reiniciar a calculadora");
-
-}
-
-if(Defdocal == "subtração"){
-    console.log("A conta de subtração foi selecionada, ela subtrai até 5 números")
-let numa = Number(prompt("digite o primeiro número a ser subtraido"));
-let numb = Number(prompt("digite o segundo número a subtrair"));
-let numc = Number(prompt("digite o terceiro número a subtrair"));
-let numd = Number(prompt("digite o quarto número a subtrair"));
-let nume = Number(prompt("digite o quinto número a subtrair"));
-const subres = numa-numb-numc-numd-nume
-alert("a subtração dos números é "+ subres)
+break;
+    
+case  Defdocal == "2" :
+    let sub = 0;
+    let nu = Number(prompt("número de números a calcular"));
+    for( i=0; i<nu; i++ ) {
+    var numero = Number(prompt("digite o número a subtrair/ser subtraido"));
+    sub = sub - numero; 
+    console.log("a subtração  total deu "+ sub)};
 console.log("programa finalizado, favor recarregar a página para reiniciar a calculadora");
-}
+break;
 
-if (Defdocal == "multiplicação"){
+case Defdocal == "3" :
     console.log ("A conta de multiplicação foi selecionada, ela multiplica até 2 números.")
-let numa = Number(prompt("digite o primeiro número a ser multiplicado"))
-let numb = Number(prompt("digite o segundo número a ser multiplicado"))
-const multres = numa*numb
+let numm1 = Number(prompt("digite o primeiro número a ser multiplicado"))
+let numm = Number(prompt("digite o segundo número a ser multiplicado"))
+const multres = numm1*numm
 alert("a multiplicação dos números deu "+ multres)
 console.log("programa finalizado, favor recarregar a página para reiniciar a calculadora");
-}
+break;
 
-if (Defdocal == "divisão"){
+
+case Defdocal == "4" :
     console.log("A conta de divisão foi selecioanda, ela divide até 2 números");
     let numa = Number(prompt("digite o número a ser dividido"));
     let numb = Number(prompt("digite o número a dividir"));
     const divres = numa/numb;
     alert("a divisão dos números deu" + divres);
-}
+    break;
+    }
